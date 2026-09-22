b651ee799101319a7cb254cec1b78c8f60ddcbaa
MiraWUSE <yahindaniil@yandex.ru>
2026-09-22 12:57:08 +0300

    update game use cases with Hilt

--- app/src/main/java/com/example/gamelib/domain/usecase/GetGamesUseCase.kt
+++ app/src/main/java/com/example/gamelib/domain/usecase/GetGamesUseCase.kt
@@ -3,8 +3,9 @@ package com.example.gamelib.domain.usecase
 import com.example.gamelib.domain.model.Game
 import com.example.gamelib.domain.repository.GameRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetGamesUseCase(
+class GetGamesUseCase @Inject constructor(
     private val repository: GameRepository
 ) {
 
