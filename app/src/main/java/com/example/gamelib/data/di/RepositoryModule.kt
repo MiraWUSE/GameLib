86452447b3b2946a8cf2c57a2fb6fe9c5b2c3a08
MiraWUSE <yahindaniil@yandex.ru>
2026-09-22 12:41:54 +0300

    create GameRepositoryImpl and bind game repository with Hilt

--- app/src/main/java/com/example/gamelib/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/gamelib/data/di/RepositoryModule.kt
@@ -1,2 +1,20 @@
 package com.example.gamelib.data.di
 
+import com.example.gamelib.data.repository.GameRepositoryImpl
+import com.example.gamelib.domain.repository.GameRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindGameRepository(
+        gameRepositoryImpl: GameRepositoryImpl
+    ): GameRepository
+}
--- app/src/main/java/com/example/gamelib/data/repository/GameRepositoryImpl.kt
+++ app/src/main/java/com/example/gamelib/data/repository/GameRepositoryImpl.kt
@@ -1,2 +1,33 @@
 package com.example.gamelib.data.repository
 
+import com.example.gamelib.data.local.dao.GameDao
+import com.example.gamelib.data.mapper.toDomain
+import com.example.gamelib.data.mapper.toEntity
+import com.example.gamelib.domain.model.Game
+import com.example.gamelib.domain.repository.GameRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+
+class GameRepositoryImpl @Inject constructor(
+    private val gameDao: GameDao
+): GameRepository {
+    override fun getAllGames(): Flow<List<Game>> {
+        return gameDao.getAllGames()
+            .map { games -> games.map { it.toDomain() }
+            }
+    }
+
+    override suspend fun addGame(game: Game) {
+        gameDao.insertGame(game.toEntity())
+    }
+
+    override suspend fun updateGame(game: Game) {
+        gameDao.updateGame(game.toEntity())
+    }
+
+    override suspend fun deleteGame(game: Game) {
+        gameDao.deleteGame(game.toEntity())
+    }
+}
